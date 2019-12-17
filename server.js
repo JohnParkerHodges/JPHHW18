0ed6a2300db01a6c425008c7ccbc4414cb3af1e7
John <phodgesfl@gmail.com>
2019-12-17 15:00:24 -0500

    L

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
     useFindAndModify: false
 });
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 // routes
 app.use(require("./routes/api.js"));
 
